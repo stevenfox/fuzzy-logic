ba5df6b222f5063b26a3725c281dca92ca351c12
stevenfox <stevenfoxmyk@gmail.com>
2020-02-09 02:16:16 +0000

    add some initial logic in the fuzzy logic set with 4 tupple

--- parser.py
+++ parser.py
@@ -16,10 +16,10 @@ def search(values, searchFor, list_index):
 
 
 def rule_set_parser():
-    filepath = file_rules
+    _filepath = file_rules
     rule_dict = {}
-    if not os.path.isfile(filepath):
-        print("File path {} does not exist. Exiting...".format(filepath))
+    if not os.path.isfile(_filepath):
+        print("File path {} does not exist. Exiting...".format(_filepath))
         sys.exit()
 
     lines = open(file_rules).readlines()
@@ -28,39 +28,89 @@ def rule_set_parser():
         (key, val) = l.strip().split(':')
         rule_dict[key] = val
 
-    dic_indx = 1
+    _dic_indx = 1
     if_word = 'If'
 
     rule_set_dict = {}
 
     for i in rule_dict:
-        if "Rule " + str(dic_indx) in i:
+        if "Rule " + str(_dic_indx) in i:
             if if_word in rule_dict[i]:
-                rule_set_dict.update({'first_var_'+str(dic_indx): search(
+                rule_set_dict.update({'first_var_'+str(_dic_indx): search(
                     rule_dict[i], if_word, 1)})
 
             rule_set_dict.update(
-                {'first_val_'+str(dic_indx): search(rule_dict[i], if_word, 3)})
+                {'first_val_'+str(_dic_indx): search(rule_dict[i], if_word, 3)})
             rule_set_dict.update(
-                {'operator_'+str(dic_indx): search(rule_dict[i], if_word, 4)})
+                {'operator_'+str(_dic_indx): search(rule_dict[i], if_word, 4)})
             rule_set_dict.update(
-                {'second_var_'+str(dic_indx): search(rule_dict[i], if_word, 5)})
+                {'second_var_'+str(_dic_indx): search(rule_dict[i], if_word, 5)})
             rule_set_dict.update(
-                {'second_val_'+str(dic_indx): search(rule_dict[i], if_word, 7)})
+                {'second_val_'+str(_dic_indx): search(rule_dict[i], if_word, 7)})
             rule_set_dict.update(
-                {'result_var_'+str(dic_indx): search(rule_dict[i], if_word, 9)})
+                {'result_var_'+str(_dic_indx): search(rule_dict[i], if_word, 9)})
             rule_set_dict.update(
-                {'result_val_'+str(dic_indx): search(rule_dict[i], if_word, 11)})
+                {'result_val_'+str(_dic_indx): search(rule_dict[i], if_word, 11)})
 
         else:
             print("No Rules found")
-        dic_indx += 1
+        _dic_indx += 1
 
-    print("set\n", rule_set_dict)
+    # print("set\n", rule_set_dict)
+
+    return rule_set_dict
+
+
+def fuzzy_set_parser():
+
+    _filepath = file_fuzzy_set
+    fuzzy_dict = {}
+    if not os.path.isfile(_filepath):
+        print("File path {} does not exist. Exiting...".format(_filepath))
+        sys.exit()
+
+    lines = open(file_fuzzy_set).readlines()
+
+    counter = 0
+    for l in lines:
+        val = l.split()
+        fuzzy_dict[counter] = val
+        counter += 1
+
+    # print(fuzzy_dict)
+
+    _dic_indx = 1
+    _tupple_indx = 1
+
+    fuzzy_set_dict = {}
+
+    for i in fuzzy_dict:
+        # print('fuzzy? :) ', _dic_indx, ' ', fuzzy_dict[i])
+        if(len(fuzzy_dict[i]) > 1):
+            fuzzy_set_dict.update(
+                {'Tuple_'+str(_tupple_indx): fuzzy_dict[i]})
+            fuzzy_4_tuple = fuzzy_dict[i][1]+fuzzy_dict[i][2] + \
+                fuzzy_dict[i][3]+fuzzy_dict[i][4]
+            # print('fuzzyy Class : ', Tupple.low)
+            _tupple_indx += 1
+        elif (len(fuzzy_dict[i]) == 1):
+            fuzzy_set_dict.update(
+                {'Title_'+str(_tupple_indx): fuzzy_dict[i]})
+            _tupple_indx += 1
+            # print('\nTitles: ', fuzzy_dict[i])
+
+        _dic_indx += 1
+
+    print(fuzzy_set_dict['Tuple_6'][0], fuzzy_set_dict['Tuple_6'][1], fuzzy_set_dict['Tuple_6'][2], fuzzy_set_dict['Tuple_6']
+          [3], fuzzy_set_dict['Tuple_6'][4])
+
+    return fuzzy_set_dict
 
 
 def main():
     rule_set_parser()
+    # print('fuzzy      ', fuzzy_set_parser())
+    fuzzy_set_parser()
 
 
 if __name__ == '__main__':
