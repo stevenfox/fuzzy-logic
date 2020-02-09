7aa42d782048e472a941835062405beaab176312
stevenfox <stevenfoxmyk@gmail.com>
2020-02-09 15:32:51 +0000

    finalise membership function

--- parser.py
+++ parser.py
@@ -72,21 +72,21 @@ class Fuzzy_set:
     def get(self):
         return self.data
 
-    def get_titles(self, dic):
+    def get_titles(self, dic, keytitle='Title_'):
         self.title_dict = []
         for key in dic:
-            if 'Title_' in key:
+            if keytitle in key:
                 self.title_dict.append(dic[key])
         return self.title_dict
 
-    def get_tuples(self, dic, title, tuples_range=3):
+    def get_tuples(self, dic, title, keytitle='Title_', tuples_range=3):
         self.tuple_dict = []
         count = 0
 
         for key in dic:
             # print()
             count += 1
-            if 'Title_' in key:
+            if keytitle in key:
                 list_tuple = list(dic.values())
                 tuple_title = ' '.join(map(str, dic[key]))
 
@@ -134,6 +134,45 @@ def fuzzy_set_parser():
     return fuzzy_set_dict
 
 
+def membership_set_parser():
+
+    _filepath = file_membership
+    membership_dict = {}
+    # f_set = Fuzzy_set()
+
+    if not os.path.isfile(_filepath):
+        print("File path {} does not exist. Exiting...".format(_filepath))
+        sys.exit()
+
+    lines = open(file_membership).readlines()
+    counter = 0
+
+    for l in lines:
+
+        str_l = l.replace('=', '')
+        val = str_l.split()
+
+        membership_dict[counter] = val
+        counter += 1
+
+    _tupple_indx = 1
+    membership_set_dict = {}
+
+    for i in membership_dict:
+        if(len(membership_dict[i]) > 1):
+            membership_set_dict.update(
+                {'membership_name_'+str(_tupple_indx): membership_dict[i]})
+            _tupple_indx += 1
+        elif (len(membership_dict[i]) == 1):
+            membership_set_dict.update(
+                {'membership_value_'+str(_tupple_indx): membership_dict[i]})
+            _tupple_indx += 1
+
+    # print(membership_set_dict)
+
+    return membership_set_dict
+
+
 def main():
     rule_set_parser()
     # print('fuzzy      ', fuzzy_set_parser())
@@ -150,10 +189,19 @@ def main():
     tuple_tip = x1.get_tuples(
         fuzzy_set_parser(), 'SizeOfTip')
 
-    print(titles)
-    print(tuple_journery)
-    print(tuple_driving)
-    print(tuple_tip)
+    # print(titles)
+    # print(tuple_journery)
+    # print(tuple_driving)
+    # print(tuple_tip)
+
+    membership = x1.get_titles(
+        membership_set_parser(),  'membership_')
+
+    journey_length = membership[0][1]
+    driving_quality = membership[1][1]
+
+    # print('driving_quality ', membership[0][1])
+    # print('journey_length ', membership[1][1])
 
 
 if __name__ == '__main__':
