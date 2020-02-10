a5d6f750b763b41093dc5ce56cf305b7fbec2e4d
stevenfox <stevenfoxmyk@gmail.com>
2020-02-10 22:28:22 +0000

    quick fix to read all the data in one file

--- file_parser.py
+++ file_parser.py
@@ -2,12 +2,13 @@ import sys
 import os
 
 
-file_rules = 'files/rules.txt'
-file_fuzzy_set = 'files/fuzzy_set.txt'
-file_membership = 'files/membership_var.txt'
+file_rules = 'files/RuleSet.txt'
+file_fuzzy_set = 'files/RuleSet.txt'
+file_membership = 'files/RuleSet.txt'
 
 
 def search(values, searchFor, list_index):
+    # print('search: ', values, searchFor, list_index)
     list_values = values.split()
     for i in list_values:
         if searchFor in i:
@@ -25,32 +26,32 @@ def rule_set_parser():
     lines = open(file_rules).readlines()
 
     for l in lines:
-        (key, val) = l.strip().split(':')
-        rule_dict[key] = val
-
+        # (key, val) = l.strip().split(':')
+        # rule_dict[key] = val
+        # (key, val) = l.strip().split(':')
+        # rule_dict[key] = val
+        rule_dict[l] = l
     _dic_indx = 1
     if_word = 'If'
 
     rule_set_dict = {}
-
     for i in rule_dict:
-        if "Rule " + str(_dic_indx) in i:
+        if "Rule " in i:
             if if_word in rule_dict[i]:
                 rule_set_dict.update({'first_var_'+str(_dic_indx): search(
-                    rule_dict[i], if_word, 1)})
-
+                    rule_dict[i], if_word, 3)})
             rule_set_dict.update(
-                {'first_val_'+str(_dic_indx): search(rule_dict[i], if_word, 3)})
+                {'first_val_'+str(_dic_indx): search(rule_dict[i], if_word, 5)})
             rule_set_dict.update(
-                {'operator_'+str(_dic_indx): search(rule_dict[i], if_word, 4)})
+                {'operator_'+str(_dic_indx): search(rule_dict[i], if_word, 6)})
             rule_set_dict.update(
-                {'second_var_'+str(_dic_indx): search(rule_dict[i], if_word, 5)})
+                {'second_var_'+str(_dic_indx): search(rule_dict[i], if_word, 7)})
             rule_set_dict.update(
-                {'second_val_'+str(_dic_indx): search(rule_dict[i], if_word, 7)})
+                {'second_val_'+str(_dic_indx): search(rule_dict[i], if_word, 9)})
             rule_set_dict.update(
-                {'result_var_'+str(_dic_indx): search(rule_dict[i], if_word, 9)})
+                {'result_var_'+str(_dic_indx): search(rule_dict[i], if_word, 11)})
             rule_set_dict.update(
-                {'result_val_'+str(_dic_indx): search(rule_dict[i], if_word, 11)})
+                {'result_val_'+str(_dic_indx): search(rule_dict[i], if_word, 13)})
 
         else:
             print("No Rules found")
@@ -84,7 +85,7 @@ class Fuzzy_set:
         count = 0
 
         for key in dic:
-            # print()
+            # print(dic)
             count += 1
             if keytitle in key:
                 list_tuple = list(dic.values())
@@ -98,6 +99,20 @@ class Fuzzy_set:
             print('No tupple title found')
 
         return self.tuple_dict
+
+    def get_numbers_tuple(self, dic, _cnt=3):
+        # print(max(dic[0][1]))
+        self.tuple_int = []
+        for k in range(_cnt):
+            self.tuple_int.append((dic[k][1:]))
+        print(self.tuple_int)
+        return self.tuple_int
+
+    def max_tuple(self, dic):
+        self.max = max(sorted(dic, key=lambda x: x[1], reverse=True)[0])
+        # print(max(sorted(dic, key=lambda x: x[1], reverse=True))[:])
+        # print(max(sorted(dic, key=lambda x: x[1], reverse=True)[0]))
+        return self.max
 
 
 def fuzzy_set_parser():
@@ -147,61 +162,71 @@ def membership_set_parser():
     lines = open(file_membership).readlines()
     counter = 0
 
+    membership_set_dict = []
     for l in lines:
 
-        str_l = l.replace('=', '')
-        val = str_l.split()
-
-        membership_dict[counter] = val
-        counter += 1
+        if '=' in l:
+            # print('GOTSA ', l)
+            # print('>>>?', lines)
+            str_l = l.replace('=', '')
+            val = str_l.split()
+            # membership_set_dict.append(l)
+            # print('val ', val)
+            # membership_set_dict.remove
+            membership_dict[counter] = val
+            counter += 1
+            # print('mem? ', membership_dict)
 
     _tupple_indx = 1
-    membership_set_dict = {}
 
-    for i in membership_dict:
-        if(len(membership_dict[i]) > 1):
-            membership_set_dict.update(
-                {'membership_name_'+str(_tupple_indx): membership_dict[i]})
-            _tupple_indx += 1
-        elif (len(membership_dict[i]) == 1):
-            membership_set_dict.update(
-                {'membership_value_'+str(_tupple_indx): membership_dict[i]})
-            _tupple_indx += 1
+    # for i in membership_dict:
+    #     if(len(membership_dict[i]) > 1):
+    #         membership_set_dict.update(
+    #             {'membership_name_'+str(_tupple_indx): membership_dict[i]})
+    #         _tupple_indx += 1
+    #     elif (len(membership_dict[i]) == 1):
+    #         membership_set_dict.update(
+    #             {'membership_value_'+str(_tupple_indx): membership_dict[i]})
+    #         _tupple_indx += 1
 
-    # print(membership_set_dict)
+    # print('?? :O', membership_set_dict)
+    # print('?? :O', membership_set_dict)
 
-    return membership_set_dict
+    return membership_dict
 
 
 def main():
-    rules_set = rule_set_parser()
+    # rules_set = rule_set_parser()
 
-    # print('rules      ', rules_set['first_val_1'])
+    # print('rules      ', rules_set)
 
     x1 = Fuzzy_set()
 
     # print(x1.add(fuzzy_set_parser()))
     titles = x1.get_titles(fuzzy_set_parser())
 
-    tuple_journery = x1.get_tuples(
-        fuzzy_set_parser(), 'JourneyLength')
-    tuple_driving = x1.get_tuples(
-        fuzzy_set_parser(), 'DrivingQuality')
-    tuple_tip = x1.get_tuples(
-        fuzzy_set_parser(), 'SizeOfTip')
+    # tuple_journery = x1.get_tuples(
+    #     fuzzy_set_parser(), 'journey_time')
+    # tuple_driving = x1.get_tuples(
+    #     fuzzy_set_parser(), 'driving')
+    # tuple_tip = x1.get_tuples(
+    #     fuzzy_set_parser(), 'tip')
 
     # print(titles)
-    # print(tuple_journery)
     # print(tuple_driving)
+    # print(tuple_journery)
     # print(tuple_tip)
 
-    membership = x1.get_titles(
-        membership_set_parser(),  'membership_')
+    # membership = x1.get_titles(
+    #     membership_set_parser(),  'membership_')
 
-    journey_length = membership[0][1]
-    driving_quality = membership[1][1]
+    # journey_length = membership[0][1]
+    # driving_quality = membership[1][1]
 
-    # print('driving_quality ', membership[0][1])
+    # print('???', membership_set_parser())
+    membership = membership_set_parser()
+
+    print('>>? ', membership[0][0])
     # print('journey_length ', membership[1][1])
 
 
